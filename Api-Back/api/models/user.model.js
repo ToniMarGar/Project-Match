6a3f827b678a8e749aaa4b445feada9e9b1bfef1
ToniMarGar <amartin7893@gmail.com>
2024-06-17 16:11:01 +0100

    Merge

---
+++ Api-Back/api/models/user.model.js
@@ -0,0 +1,38 @@
+const { sequelize } = require("../../database/index.js");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define(
+    "user",
+    {
+    username: {
+    type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    surname: {
+      type: DataTypes.STRING,
+      unique: true,
+     },
+    role: {
+      type: DataTypes.ENUM("Admin", "Client"),
+      defaultValue:"Client",
+      allowNull: false,
+     },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+     },
+   password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+     },
+    },
+    {
+    	//opciones
+      createdAt:false,
+    	updatedAt: false,
+    	}
+);
+
+module.exports = User;
---
+++ Api-Back/api/routes/user.router.js
@@ -0,0 +1,9 @@
+const { getOneUser } = require("../controllers/user.controller.js");
+const { checkAuth,checkAdmin } = require("../middleware/index.js");
+
+const router = require('express').Router() 
+
+router.get("/:id",checkAuth,checkAdmin,getOneUser);
+
+
+module.exports = router
