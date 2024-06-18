336963a671ffa17daf4dba4ae1c0b048abcbab83
karen-rms <karen.ramos.perdomo.rp@gmail.com>
2024-06-18 10:26:56 +0100

    API-Back. - CRUD en controller - Conexiones en router

--- Api-Back/api/routes/user.router.js
+++ Api-Back/api/routes/user.router.js
@@ -1,9 +1,22 @@
 const { getOneUser } = require("../controllers/user.controller.js");
 const { checkAuth,checkAdmin } = require("../middleware/index.js");
 
+const {
+    getOneUser,
+    getAllUsers,
+    createUser,
+    updateUser,
+    deleteUser
+  } = require("../controllers/user.controller.js");
+const { checkAuth,checkAdmin } = require("../middleware/index.js");
+
 const router = require('express').Router() 
 
 router.get("/:id",checkAuth,checkAdmin,getOneUser);
+router.get("/:id",checkAuth,checkAdmin,getAllUsers);
+router.get("/:id",checkAuth,checkAdmin,createUser);
+router.get("/:id",checkAuth,checkAdmin,updateUser);
+router.get("/:id",checkAuth,checkAdmin,deleteUser);
 
 
 module.exports = router
