da7dfafedd5bb0a9101a4dbee2db361ff19d2ce4
karen-rms <karen.ramos.perdomo.rp@gmail.com>
2024-06-21 11:51:59 +0100

    FRONT: - RouterProvider added - router fold & content added - routing test in header

--- React-Front/src/main.jsx
+++ React-Front/src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import {RouterProvider} from 'react-router-dom'
+import router from './router/index'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <App />
-      <Footer />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
