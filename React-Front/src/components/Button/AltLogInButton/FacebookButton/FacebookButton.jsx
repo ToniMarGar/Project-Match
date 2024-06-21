2a5e2ed2443e54b688ad85429e6b18b4339c862b
karen-rms <karen.ramos.perdomo.rp@gmail.com>
2024-06-21 12:23:52 +0100

    FRONT: - layout css structure - LogIn page fixed

--- React-Front/src/components/Button/AltLogInButton/FacebookButton/FacebookButton.jsx
+++ React-Front/src/components/Button/AltLogInButton/FacebookButton/FacebookButton.jsx
@@ -1,15 +1,20 @@
 import * as React from "react";
+
+import Facebook from '../../../../assets/start-session-logos/facebook.png'
 import '../CommonStyle.css'
 
 function LogFacebookButton() {
   return (
-      <button>
+    <Link href='https://www.facebook.com/login/'>
+      <button className='other-signout'>
         <img 
+          className='icon-signout'
           loading="lazy" 
-          src="../../../../assets/start-session-logos/facebook.png" 
+          src={Facebook}
           alt="Log in with Facebook" 
         />
       </button>
+    </Link>
   );
 }
 
