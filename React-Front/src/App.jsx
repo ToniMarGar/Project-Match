7aae62068ca6cbb1b1d56a1e99b1975556de2c5d
FreddyLopez208 <freddylopez208@gmail.com>
2024-06-25 09:44:47 +0100

    preba

--- React-Front/src/App.jsx
+++ React-Front/src/App.jsx
@@ -2,20 +2,13 @@ import React from 'react'
 import './App.css'
 
 import Home from './pages/Home/Home'
-import Quizz from './pages/Quizz/Quizz'
-import NotFound from './pages/NotFound/NotFound'
-import FormLogin from './components/FormLogIn'
-import FormSignup from './components/FormSignup'
-import Profile from './pages/Profile/Profile'
-import InfoUser from './components/InfoUser'
 
 
 function App() {
 
   return (
     <>
-      <FormLogin />
-      <InfoUser />
+      <NotFound />
     </>
   )
 }
